305295c6cd14a4b136ababc539dc9ed99a62a01d
zedwardsorrauto-cmyk <zedwards.orrauto@gmail.com>
2026-08-14 11:56:20 -0500

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,8 @@ app = FastAPI(title="TractorCloser API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
